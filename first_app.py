4c7f8256c6edc1923fa6417e832774d27145eabd
unknown <ashakur.ahamid@gmail.com>
2021-11-08 19:36:56 +0300

    Updated commit

--- first_app.py
+++ first_app.py
@@ -10,38 +10,41 @@ st.title('my frist app ')
 # write text
 st.write('i love this ')
 
+st.text('Fixed width text')
+st.markdown('_Markdown_') # see *
+st.latex(r''' e^{i\pi} + 1 = 0 ''')
+st.write('Most objects') # df, err, func, keras!
+st.write(['st', 'is <', 3]) # see *
+st.title('My title')
+st.header('My header')
+st.subheader('My sub')
+st.code('for i in range(8): foo()')
 
-# data frame
 st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
 }))
-
-# chart
+st.table(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
 chart_data = pd.DataFrame(
      np.random.randn(20, 3),
      columns=['a', 'b', 'c'])
 
 st.line_chart(chart_data)
-
 # map
 map_data = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
     columns=['lat', 'lon'])
 
 st.map(map_data)
-
-
-# use check box to show or hide data
 if st.checkbox('Show dataframe'):
     chart_data = pd.DataFrame(
        np.random.randn(20, 3),
        columns=['a', 'b', 'c'])
 
     chart_data
-
-# select box
-
 df=pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
@@ -50,27 +53,20 @@ df=pd.DataFrame({
 option = st.selectbox('Which number do you like best?',df['first column'])
 
 'You selected: ', option
-
-
 # side bar
 st.sidebar.selectbox(
     'Which number do you like best?',
      df['second column'])
-
-# side by side widget
-left_column, right_column = st.beta_columns(2)
+left_column, right_column = st.columns(2)
 pressed = left_column.button('Press me?')
 if pressed:
     right_column.write("Woohoo!")
 
-expander = st.beta_expander("FAQ")
+expander = st.expander("FAQ")
 expander.write("Here you could put in some really, really long explanations...")
-
-
-#progress
+st.balloons()
 import time
 'Starting a long computation...'
-
 # Add a placeholder
 latest_iteration = st.empty()
 bar = st.progress(0)
@@ -81,8 +77,4 @@ for i in range(100):
   bar.progress(i + 1)
   time.sleep(0.1)
 
-'...and now we\'re done!'
-
-
-#share app
-#pip freeze > requirements.txt
+  #pip freeze > requirements.txt
